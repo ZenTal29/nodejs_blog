a7180b4c660a02a18630f150f000b367cfb33c32
ZenTal29 <quanluong4646@gmail.com>
2022-03-07 12:25:51 +0700

    update layout css

---
+++ src/index.js
@@ -0,0 +1,28 @@
+const path = require('path')
+const express = require('express')
+const morgan = require('morgan')
+const { engine } = require ('express-handlebars');
+const app = express()
+const port = 3000
+
+app.use(express.static(path.join(__dirname, 'public')));
+
+// HTTP logger
+app.use(morgan('combined'))
+
+// Template engine
+app.engine('handlebars', engine())
+app.set('view engine', 'handlebars');
+app.set('views', path.join(__dirname, 'resources/views'))
+
+app.get('/', (req, res) => {
+  res.render('home');
+});
+
+app.get('/new', (req, res) => {
+  res.render('new');
+});
+
+app.listen(port, () => {
+  console.log(`Example app listening at http://localhost:${port}/trang-chu`)
+})
